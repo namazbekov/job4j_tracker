4d78d2cf0892aca3eb2445d33399a282d3fc7112
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-05-12 21:32:40 +0300

    1.7. Конструктор [#173889 #132437]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error(){
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.message = message;
+        this.status = status;
+        this.active = active;
+    }
+
+    public static void main(String[] args) {
+        Error first = new Error(true, 1, "Где ты был?");
+        first.printInfo();
+        Error second = new Error();
+        second.printInfo();
+    }
+
+    public void printInfo() {
+        System.out.println("Активность : "+ active);
+        System.out.println("Сколько минут был онлайн : "+ status + " минут");
+        System.out.println("Сообщения : " + message);
+
+    }
+}
