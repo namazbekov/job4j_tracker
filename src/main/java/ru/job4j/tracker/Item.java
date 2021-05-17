0c47886f289542b96d31d79d553c8e806ae0a6f1
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-05-17 14:57:17 +0300

    5.1. Зона видимости переменных [#511879]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,6 +4,15 @@ public class Item {
     private int id;
     private String name;
 
+    public void Idem(int id) {
+        this.id = id;
+    }
+
+    public void Idem(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public int getId() {
         return id;
     }
