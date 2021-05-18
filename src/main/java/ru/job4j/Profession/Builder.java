42c05a5e554fbb7fae47ce27b7294296fb2709f9
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-05-18 14:59:26 +0300

    3. Реализация профессий в коде [#511888]

--- src/main/java/ru/job4j/Profession/Builder.java
+++ src/main/java/ru/job4j/Profession/Builder.java
@@ -3,14 +3,12 @@ package ru.job4j.Profession;
 public class Builder extends Engineer {
     private int build;
 
-    public void toBuild (int house) {
-        this.build = house;
-    }
-    public String name() {
-        return super.getName();
-    }
-
     public int getBuild() {
         return this.build;
+    }
+
+    @Override
+    public String getBirthday() {
+        return super.getBirthday();
     }
 }
