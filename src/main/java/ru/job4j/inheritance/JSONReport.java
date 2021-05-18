3646aa621cf8b1ea940a19d4f465caf03ad63f1d
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-05-18 12:36:54 +0300

    4. Переопределение [#511884]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,6 +2,9 @@ package ru.job4j.inheritance;
 
 public class JSONReport {
     public String generate(String name, String body) {
-        return "name: "+ name + System.lineSeparator() + "body: "+ body;
+        return "{" + System.lineSeparator() +
+                "name: "+ name + System.lineSeparator() +
+                "body: "+ body + System.lineSeparator() +
+                "}";
     }
 }
