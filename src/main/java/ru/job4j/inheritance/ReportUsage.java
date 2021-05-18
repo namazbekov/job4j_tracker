9f1d76c84123bfbf893ae7813939b46abee46d3c
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-05-18 15:13:01 +0300

    5. Аннотация @Override [#511885]

--- src/main/java/ru/job4j/inheritance/ReportUsage.java
+++ src/main/java/ru/job4j/inheritance/ReportUsage.java
@@ -1,6 +1,7 @@
 package ru.job4j.inheritance;
 
 public class ReportUsage {
+
     public static void main(String[] args) {
         HtmlReport report = new HtmlReport();
         JSONReport print = new JSONReport();
