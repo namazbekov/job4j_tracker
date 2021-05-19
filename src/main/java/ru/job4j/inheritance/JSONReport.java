b953c4ae38632c32426ffdf5a81fa81f62f5a4d3
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-05-19 14:09:13 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -2,7 +2,7 @@ package ru.job4j.inheritance;
 
 public class JSONReport {
 
-    @Override
+
     public String generate(String name, String body) {
         return "{" + System.lineSeparator() +
                 "name: "+ name + "," + System.lineSeparator() +
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,16 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class Item {
     private int id;
     private String name;
-    private String LocalDateTime;
+    private LocalDateTime created = LocalDateTime.now();
 
-    public void Idem() {
+    public void Idem(LocalDateTime created) {
+        this.created = created;
     }
 
     public void Idem(String name) {
@@ -15,6 +20,9 @@ public class Item {
     public void Idem(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
