aba25ea51532c0a57d175db7810ced7448133012
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-05-26 13:01:48 +0300

    1. Чтение из консоли. Класс Scanner. [#179514 #160846]

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -11,6 +11,8 @@ public class MagicBall {
             String name = input.nextLine();
             int answer = new Random().nextInt(3);
             if (name.equals("end") || name.equals("хватит")) {
+                System.out.println("Всем спасибо, всем пока!");
+                break;
             } else if (answer == 1) {
                 System.out.println("Нет");
             } else if (answer == 0) {
