92a33ee4e33debe3e7e8211658dce17c35b612d7
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-06-05 15:02:33 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,8 @@
+package ru.job4j.ex;
+
+public class ElementNotFoundException extends Exception{
+
+    public ElementNotFoundException(String massage) {
+        super(massage);
+    }
+}
---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+public class FindEl {
+
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                rsl = index;
+                break;
+            } else {
+                throw new ElementNotFoundException("Нет элемента");
+            }
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] str = {"puma", "addidas", "nike"};
+        try {
+            System.out.println(FindEl.indexOf(str, "nike"));
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
