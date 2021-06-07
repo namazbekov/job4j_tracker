a3790b3b3002ca86dfeb30a5dc2cba51889a85e6
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-06-07 13:52:03 +0300

    2. Singleton [#94606]

---
+++ src/main/java/ru/job4j/SingleTracker.java
@@ -0,0 +1,42 @@
+package ru.job4j;
+
+import ru.job4j.tracker.Item;
+import ru.job4j.tracker.Tracker;
+
+public final class SingleTracker {
+    private static SingleTracker instance;
+    private Tracker tracker = new Tracker();
+
+    private SingleTracker() {
+        tracker = new Tracker();
+    }
+
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
+        }
+        return instance;
+    }
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+    public Item[] findAll() {
+        return tracker.findAll();
+    }
+
+    public Item findById(int id) {
+        return tracker.findById(id);
+    }
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
+    }
+    private int indexOf(int id) {
+        return tracker.indexOf(id);
+    }
+    public boolean replace(int id, Item item) {
+        return tracker.replace(id,item);
+    }
+    public boolean delete(int id) {
+        return tracker.delete(id);
+    }
+}
