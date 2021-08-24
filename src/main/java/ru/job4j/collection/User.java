7651d25cc99f26dd2bf0df28463c92fe94a28ea8
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-08-24 17:25:47 +0300

    0. Сортировка [#10096 #193724]

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,52 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int result = this.name.compareTo(o.getName());
+        if(result == 0){
+            result = Integer.compare(this.age, o.age);
+        }
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age &&
+                Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
