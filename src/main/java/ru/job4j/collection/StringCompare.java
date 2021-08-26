d4b18b35386205d9a5c00d124b70956ce62a32b3
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-08-26 23:19:09 +0300

    3. Компаратор для строк [#35008 #194901]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,8 +5,9 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String str1, String str2) {
-        for (int i = 0; i < str1.length() && i < str2.length(); i++) {
-            if ((int) str1.charAt(i) != (int) str2.charAt(i)) {
+        int min = Math.min(str1.length(), str2.length());
+        for (int i = 0; i < min; i++) {
+            if ( str1.charAt(i) !=  str2.charAt(i)) {
                 return Character.compare(str1.charAt(i), str2.charAt(i));
             }
         }
