91408d78f27bf041436b5fc7a440238e683ac1a8
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-08-26 23:08:17 +0300

    4. Сортировка номера [#221284 #194961]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -1,19 +1,16 @@
 package ru.job4j.collection;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        String left1 = null;
-        String right1 = null;
-        for (int i = 0; i < left.length() && i < right.length(); i++) {
-            if (left.charAt(i) > 0 || right.charAt(i) > 0) {
-                left1 = String.valueOf(left.charAt(i));
-                right1 = String.valueOf(right.charAt(i));
-            }
-        }
-        return left1.compareTo(right1);
+        String[] regex1 = left.split("\\. ");
+        String[] regex2 = right.split("\\. ");
+        int i = Integer.parseInt(String.valueOf(regex1.length));
+        int j = Integer.parseInt(String.valueOf(regex2.length));
+        return Integer.compare(i,j);
     }
 }
