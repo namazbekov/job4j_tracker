025a5b9702fc5e0f8fb89af9dc5e041ae9a70afc
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-08-26 13:01:38 +0300

    4. Сортировка номера [#221284]

--- src/main/java/ru/job4j/collection/JobByName.java
+++ src/main/java/ru/job4j/collection/JobByName.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class JobByName implements Comparator<Job> {
     @Override
     public int compare(Job o1, Job o2) {
-        return o2.getName().compareTo(o1.getName());
+        return o1.getName().compareTo(o2.getName());
     }
 }
