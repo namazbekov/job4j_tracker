7330e25e412cb5e7467936921a23aed92e583ef7
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-08-30 14:01:15 +0300

    ффффффф

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,13 +5,33 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Класс описывает работу банковского перевода денег с одного расчетного счета на другоой
+ * @author Дастан
+ * @version 1.0
+ */
+
 public class BankService {
+    /**
+     * хранение пользователей осуществляется в карте Map с типом User и List с типом Account
+     */
     private Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * метод добавляет пользователя в систему, через метод put
+     * @param user
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Этот метод принимает два параметра пользователя и счета
+     * , ищет этого пользователя по реализовванному в другом методде поиску и ищет совпадения с счетами
+     * , если нет идет процесс добавления пользователя
+     * @param passport
+     * @param account
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -23,6 +43,12 @@ public class BankService {
         }
     }
 
+    /**
+     * метод ищет пользователя по имени, ищет в списке users с помшью foreach
+     * , и если есть совпадения возвращает имя пользователя, в противном случае возварщает null
+     * @param passport
+     * @return
+     */
     public User findByPassport(String passport) {
         for (User us : users.keySet()) {
             if (us.getPassport().equals(passport)) {
@@ -32,6 +58,17 @@ public class BankService {
         return null;
     }
 
+    /**
+     * метод ищет пользователя с помощью двух параметров имени и реквизитов
+     * , сначала идет поиск по имени  и если пользователь есть
+     * , получаем весь список для поиска по реквизитам
+     * , еслие есть сходство получаем список расчетных счетов пользователя
+     * в противном случае возвращаем null
+     *
+     * @param passport
+     * @param requisite
+     * @return
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -46,6 +83,18 @@ public class BankService {
         return null;
     }
 
+    /**
+     * метод отправки денег с расчетного счета на другой счет осуществляется по двум пользователям
+     * ,которых мы должны нати в списке с помошью принятых пяти параметров имен и реквизитов и amount.
+     * после идет проверка этих пользователей на нераность null и положительного баланса на счету
+     * и если будет истина , то идет процесс уменьшения баланса одного пользователя и увеличение другого.
+     * @param srcPassport
+     * @param srcRequisite
+     * @param destPassport
+     * @param destRequisite
+     * @param amount
+     * @return
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
