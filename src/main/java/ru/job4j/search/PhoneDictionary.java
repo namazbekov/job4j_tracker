e178e865045d362afec6dd5e2978dfa23c45344e
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-09-17 22:27:59 +0300

    5. Подсчет функции в диапазоне. [#24251 #202129]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
 
@@ -11,10 +12,15 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<Person>();
+        Predicate<Person> combine1 = (person) -> person.getName().contains(key);
+        Predicate<Person> combine2 = (person) -> person.getSurname().contains(key);
+        Predicate<Person> combine3 = (person) -> person.getAddress().contains(key);
+        Predicate<Person> combine4 = (person) -> person.getPhone().contains(key);
+        Predicate<Person> combine = combine1.or(person -> combine2.test(person)
+                && combine3.test(person) && combine4.test(person));
+        ArrayList<Person> result = new ArrayList<>();
         for (Person person: persons) {
-            if (person.getName().contains(key) || person.getSurname().contains(key)
-                    || person.getAddress().contains(key) || person.getPhone().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
