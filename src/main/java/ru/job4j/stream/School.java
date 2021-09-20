44e70c45af69679957a7cb0dc7ca8518851862ac
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-09-20 07:13:24 +0300

    1. Фильтрация учеников [#110058]

--- src/main/java/ru/job4j/stream/School.java
+++ src/main/java/ru/job4j/stream/School.java
@@ -3,15 +3,11 @@ package ru.job4j.stream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class School {
     public List<Student> collect(List<Student> students, Predicate<Student> predict) {
-        List<Student> rsl = new ArrayList<>();
-        for (Student stud : students) {
-            if (predict.test(stud)) {
-                rsl.add(stud);
-            }
-        }
+        List<Student> rsl = students.stream().filter(predict).collect(Collectors.toList());
         return rsl;
     }
 }
--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.function.Predicate;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -19,13 +19,13 @@ public class SchoolTest {
                 new Student(70, "Surname7"),
                 new Student(90, "Surname9")
         );
-        List<Student> collectA = students.stream()
-                .filter(student -> student.getScore() >= 70)
-                .collect(Collectors.toList());
+        School sc = new School();
+        Predicate<Student> pr = student -> student.getScore() >= 70;
+        List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(70, "Surname7"));
         expected.add(new Student(90, "Surname9"));
-        assertThat(collectA, is(expected));
+        assertThat(rsl, is(expected));
     }
 
     @Test
@@ -37,14 +37,13 @@ public class SchoolTest {
                 new Student(60, "Surname6"),
                 new Student(80, "Surname8")
         );
-        List<Student> collectB = students.stream()
-                .filter(student -> student.getScore() >= 50)
-                .filter(student -> student.getScore() < 70)
-                .collect(Collectors.toList());
+        School sc = new School();
+        Predicate<Student> pr = student -> student.getScore() >= 50 &&  student.getScore() < 70;
+        List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(50, "Surname5"));
         expected.add(new Student(60, "Surname6"));
-        assertThat(collectB, is(expected));
+        assertThat(rsl, is(expected));
     }
 
     @Test
@@ -56,13 +55,13 @@ public class SchoolTest {
                 new Student(60, "Surname6"),
                 new Student(90, "Surname9")
         );
-        List<Student> collectC = students.stream()
-                .filter(student -> student.getScore() < 50)
-                .collect(Collectors.toList());
+        School sc = new School();
+        Predicate<Student> pr = student -> student.getScore() < 50;
+        List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(10, "Surname1"));
         expected.add(new Student(30, "Surname3"));
         expected.add(new Student(40, "Surname4"));
-        assertThat(collectC, is(expected));
+        assertThat(rsl, is(expected));
     }
 }
