24a67409617606e13c17b871064c830f83ba0d08
namazbekov <82052352+namazbekov@users.noreply.github.com>
2021-10-11 20:44:21 +0300

    2. Аттестация [#49777]

---
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,55 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream
+                .flatMap(str -> str.getSubjects().stream())
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0);
+    }
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+        return stream.map(str -> new Tuple(str.getName(), str.getSubjects()
+                .stream()
+                .mapToInt(Subject::getScore)
+                .average()
+                .orElse(0)))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return stream
+                .flatMap(str -> str.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject :: getName
+                        , Collectors.averagingDouble(Subject :: getScore)))
+                .entrySet()
+                .stream()
+                .map(str -> new Tuple(str.getKey(), str.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return stream.map(str -> new Tuple(str.getName(), str.getSubjects()
+                .stream()
+                .mapToInt(Subject::getScore).sum())).max(Tuple::compareTo).orElse(new Tuple(" ", 0))
+                ;
+    }
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return stream
+                .flatMap(str -> str.getSubjects().stream())
+                .collect(Collectors.groupingBy(Subject :: getName
+                        , Collectors.summingDouble(Subject :: getScore)))
+                .entrySet()
+                .stream()
+                .map(str -> new Tuple(str.getKey(), str.getValue()))
+                .max(Tuple::compareTo)
+                .orElse(new Tuple(" ", 0));
+    }
+}
