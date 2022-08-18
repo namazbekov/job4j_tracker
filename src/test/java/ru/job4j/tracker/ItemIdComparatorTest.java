5edbdedf11c2d816df29ce51e0d70f6202b1bf55
user1 <dastan.namazbekov@mail.ru>
2022-08-19 04:26:52 +0600

    2. Трекер SQL [#1734]

--- src/test/java/ru/job4j/tracker/ItemIdComparatorTest.java
+++ src/test/java/ru/job4j/tracker/ItemIdComparatorTest.java
@@ -1,33 +1,37 @@
-////package ru.job4j.tracker;
-////
-////
-////import org.junit.Test;
-////
-////import java.util.ArrayList;
-////import java.util.Arrays;
-////import java.util.Collections;
-////import java.util.List;
-////
-////import static org.hamcrest.core.Is.is;
-////import static org.junit.Assert.assertThat;
-////
-////public class ItemIdComparatorTest {
-////
-////    @Test
-////    public void comparable() {
-////        List<Item> list = new ArrayList<>();
-////        Item item1 = new Item(4, "bmv", );
-////        Item item2 = new Item(2, "mazda");
-////        Item item3 = new Item(6, "mercedes");
-////        list.add(item3);
-////        list.add(item2);
-////        list.add(item1);
-////        Collections.sort(list, new ItemIdComparator());
-////        List<Item> expected = new ArrayList<>();
-////        expected.add(item1);
-////        expected.add(item2);
-////        expected.add(item3);
-////        assertThat(list, is(expected));
-////
-////    }
-//}
+package ru.job4j.tracker;
+
+
+import org.junit.Test;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ItemIdComparatorTest {
+
+    @Test
+    public void comparable() {
+        long millis = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(millis);
+        LocalDateTime localDateTime = timestamp.toLocalDateTime();
+        List<Item> list = new ArrayList<>();
+        Item item1 = new Item(4, "bmv", localDateTime );
+        Item item2 = new Item(2, "mazda", localDateTime);
+        Item item3 = new Item(6, "mercedes", localDateTime);
+        list.add(item3);
+        list.add(item2);
+        list.add(item1);
+        Collections.sort(list, new ItemIdComparator());
+        List<Item> expected = new ArrayList<>();
+        expected.add(item1);
+        expected.add(item2);
+        expected.add(item3);
+        assertThat(list, is(expected));
+
+    }
+}
--- src/test/java/ru/job4j/tracker/ItemNameComparatorTest.java
+++ src/test/java/ru/job4j/tracker/ItemNameComparatorTest.java
@@ -1,29 +1,34 @@
-//package ru.job4j.tracker;
-//
-//import org.junit.Test;
-//import ru.job4j.collection.FullSearch;
-//import ru.job4j.collection.Task;
-//
-//import java.util.*;
-//
-//import static org.hamcrest.core.Is.is;
-//import static org.junit.Assert.assertThat;
-//
-//public class ItemNameComparatorTest {
-//    @Test
-//    public void toNameTest() {
-//        List<Item> list = new ArrayList<>();
-//        Item item1 = new Item(4, "bmv");
-//        Item item2 = new Item(2, "mazda");
-//        Item item3 = new Item(6, "mercedes");
-//        list.add(item1);
-//        list.add(item2);
-//        list.add(item3);
-//        Collections.sort(list, new ItemNameComparator());
-//        List<Item> expected = new ArrayList<>();
-//        expected.add(item3);
-//        expected.add(item2);
-//        expected.add(item1);
-//        assertThat(list, is(expected));
-//    }
-//}
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.collection.FullSearch;
+import ru.job4j.collection.Task;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ItemNameComparatorTest {
+    @Test
+    public void toNameTest() {
+        long millis = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(millis);
+        LocalDateTime localDateTime = timestamp.toLocalDateTime();
+        List<Item> list = new ArrayList<>();
+        Item item1 = new Item(4, "bmv", localDateTime);
+        Item item2 = new Item(2, "mazda", localDateTime);
+        Item item3 = new Item(6, "mercedes", localDateTime);
+        list.add(item1);
+        list.add(item2);
+        list.add(item3);
+        Collections.sort(list, new ItemNameComparator());
+        List<Item> expected = new ArrayList<>();
+        expected.add(item3);
+        expected.add(item2);
+        expected.add(item1);
+        assertThat(list, is(expected));
+    }
+}
