d77026470e3b227f3a71b12b77146df4a0f7b95e
user1 <dastan.namazbekov@mail.ru>
2022-08-26 23:43:47 +0600

    2. Liquibase. Добавление новых скриптов. [#504893]

--- src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -5,18 +5,14 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-//import ru.job4j.tracker.model.Item;
 
 import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class SqlTrackerTest {
@@ -54,11 +50,66 @@ public class SqlTrackerTest {
 
     @Test
     public void whenSaveItemAndFindByGeneratedIdThenMustBeTheSame() {
+        long millis = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(millis);
+        LocalDateTime localDateTime = timestamp.toLocalDateTime();
         SqlTracker tracker = new SqlTracker(connection);
         initConnection();
-        Item item = new Item("item");
+        Item item = new Item(1, "item",localDateTime);
         tracker.add(item);
         assertThat(tracker.findById(item.getId()), is(item));
     }
 
+    @Test
+    public void whenSaveItemAndDropItem() {
+        long millis = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(millis);
+        LocalDateTime localDateTime = timestamp.toLocalDateTime();
+        SqlTracker tracker = new SqlTracker(connection);
+        initConnection();
+        Item item = new Item(1, "item",localDateTime);
+        tracker.add(item);
+        assertThat(tracker.delete(item.getId()), is(true));
+    }
+
+    @Test
+    public void whenSaveItemAndReplaceItem() {
+        long millis = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(millis);
+        LocalDateTime localDateTime = timestamp.toLocalDateTime();
+        SqlTracker tracker = new SqlTracker(connection);
+        initConnection();
+        Item item = new Item(1, "item",localDateTime);
+        Item item2 = new Item(item.getId(), "item222",localDateTime);
+        tracker.add(item);
+        assertThat(tracker.replace(item.getId(), item2), is(true));
+    }
+
+    @Test
+    public void whenSaveItemAndRFindByNameItem() {
+        long millis = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(millis);
+        LocalDateTime localDateTime = timestamp.toLocalDateTime();
+        SqlTracker tracker = new SqlTracker(connection);
+        initConnection();
+        List<Item> list = new ArrayList<>();
+        Item item = new Item(1, "item", localDateTime);
+        tracker.add(item);
+        list.add(item);
+        assertThat(tracker.findByName("item"), is(list));
+    }
+
+    @Test
+    public void whenSaveItemAndRFindAllItems() {
+        long millis = System.currentTimeMillis();
+        Timestamp timestamp = new Timestamp(millis);
+        LocalDateTime localDateTime = timestamp.toLocalDateTime();
+        SqlTracker tracker = new SqlTracker(connection);
+        initConnection();
+        List<Item> list = new ArrayList<>();
+        Item item = new Item(1, "item", localDateTime);
+        tracker.add(item);
+        list.add(item);
+        assertThat(tracker.findAll(), is(list));
+    }
 }
